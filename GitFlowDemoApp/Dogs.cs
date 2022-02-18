e7d5eb19a03775496502932a83ef111c2a9a0c79
Marcus <medina.jobb@outlook.com>
2022-02-18 15:08:35 +0100

    Cats and dogs added

---
+++ GitFlowDemoApp/Dogs.cs
@@ -0,0 +1,18 @@
+// ----------------------------------------------------------------------------------------------
+// <copyright file="Dogs.cs" company="Marcus Medina">
+// -----------------------------------------------------------------------------------------------
+//  Dogs.cs by Marcus Medina, © Copyright (C) 2022, http://MarcusMedina.Pro.
+//  Published under Apache License 2.0 (Apache-2.0) - https://www.tldrlegal.com/l/apache2
+// </copyright>
+// -----------------------------------------------------------------------------------------------
+
+namespace GitFlowDemoApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Dogs
+{
+}
